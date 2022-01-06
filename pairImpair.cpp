ee4b9e5ce63b7c4258e07cf7c1de09437d2b0cde
Florent <91935659+FinalSlyter@users.noreply.github.com>
2022-01-06 18:48:13 +0100

    Create pairImpair.cpp

---
+++ pairImpair.cpp
@@ -0,0 +1,22 @@
+              // Afficher les nombres pairs et les nombres impairs avec des boucles
+#include <iostream>
+
+int main()
+{
+    int nombre{};
+	std::cout << "Donne-moi un nombre" << std::endl;
+    std::cin >>nombre;
+
+       
+	    if ( nombre % 2 == 0)
+        {
+         std::cout << "nombre pair" << std::endl;
+        }
+		else 
+		{
+        std::cout << "nombre impair" << std::endl;
+        }
+	
+	std::cin.get();
+    return 0;
+}
