1d3d022affcc4d51523a2436deab0f814bd0a28f
Florent <91935659+FinalSlyter@users.noreply.github.com>
2022-01-06 18:52:44 +0100

    Create Score_game.cpp

---
+++ Score_game.cpp
@@ -0,0 +1,28 @@
+                    // Avec le score d'un jeux évaluer un joueur
+#include <iostream>
+
+int main (){
+std::cout << "Quelle est le score du joueur ? " << std::endl;
+int score { 0 };
+std::cin >> score;
+
+if (score < 2000){
+    std::cout << "C’est la catastrophe!" << std::endl;
+}
+
+else if (score >= 2000 && score < 5000){
+    std::cout << "Tu peux mieux faire!" << std::endl;
+}
+
+else if (score >= 5000 && score < 9000){
+    std::cout << "Tu es sur la bonne voie" << std::endl;
+}
+
+else {
+    std::cout << "Tu es le meilleur!" << std::endl;
+}
+
+
+std::cin.get();
+return 0;    
+}
